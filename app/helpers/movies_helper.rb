6fb71cc9b8652bd6622fd137f492f71b93fc44c8
Nicholas Weber <nicholaseweber@gmail.com>
2022-02-09 11:40:16 -0600

    Add view helpers
    Chapter 7

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,14 @@
 module MoviesHelper
+
+  def total_gross(movie)
+    if movie.flop?
+      return "Flop!"
+    else
+      return number_to_currency(movie.total_gross, precision:0)
+    end
+  end
+
+  def year_of(movie)
+    movie.released_on.year rescue "Missing release date"
+  end
 end
