18a1b1ae9591d63c3c2d07ab1007cd554d5a6e1a
Nicholas Weber <nicholaseweber@gmail.com>
2022-02-11 10:05:12 -0600

    Add routes and links to app
    Chapter 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
+  root "movies#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: "movie"
 end
