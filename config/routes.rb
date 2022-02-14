afd097caeb1ee4f527b22caf4a00c55c29aa52da
Nicholas Weber <nicholaseweber@gmail.com>
2022-02-14 11:01:21 -0600

    Add new route, template and action
    Chapter 13

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
 
   root "movies#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-
-  patch "movies/:id" => "movies#update"
+  resources :movies
 
 end
