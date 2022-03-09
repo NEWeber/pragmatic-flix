ffe2c2148189aadd88bfd70a0180b5cb71b4f3b0
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-09 08:41:17 -0600

    Add authentication to seeing user index
    Chapter 33.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   add_flash_types(:danger) # registers danger as a flash type that can now be used and styled
+
+  private
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    helper_method :current_user
+
+    def require_signin
+      unless current_user
+        redirect_to new_session_path, alert: "Sign in first"
+      end
+    end
+
 end
