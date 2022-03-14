bcff6b7faaa79e8a5098bfeee8a11b67ffa90ae7
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-14 16:55:16 -0500

    Add join model linking users and reviews
    Chapter 35.1

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,18 +1,15 @@
 class Review < ApplicationRecord
   belongs_to :movie
-
-  validates :name, presence: true
+  belongs_to :user
 
   validates :comment, length: { minimum: 4 }
 
-  validates :stars, numericality: {
-    only_interger: true,
-    greater_than_or_equal_to: 1,
-    less_than_or_equal_to: 5,
+  STARS = [1,2,3,4,5]
+
+  validates :stars, inclusion: {
+    in: STARS,
     message: "must be between 1 and 5"
   }
-
-  STARS = [1,2,3,4,5]
 
   def stars_as_percent
     (stars / 5.0) * 100.0
