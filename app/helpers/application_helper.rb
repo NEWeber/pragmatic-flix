57025a24ee4f7fb2f8bbacc8b715eb42ccdc5186
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-15 09:31:40 -0500

    Update views to take user/review linking into account
    Chapter 35.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def profile_image(user, size=80)
+    url = "http://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}"
+    image_tag(url, alt: user.name)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,2 @@
 module UsersHelper
-  def profile_image(user)
-    url = "http://secure.gravatar.com/avatar/#{user.gravatar_id}"
-    image_tag(url, alt: user.name)
-  end
 end
