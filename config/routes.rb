e42713da751e2a807e8ad19f0f66e21b6fd0de06
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-15 10:50:09 -0500

    Add favorites join model
    Chapter 36

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :favorites
   root "movies#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :movies do
