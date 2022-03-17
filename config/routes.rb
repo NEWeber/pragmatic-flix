7d7be293dd19e2e9364c79d3dba6bcb7e0282353
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-17 10:03:21 -0500

    Add favorite and unfavorite to FE and associated BE changes
    Chapter 37.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :favorites
   root "movies#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :movies do
     resources :reviews
+    resources :favorites, only: [:create, :destroy]
   end
 
   resource :session, only: [:new, :create, :destroy]
