3231422e68d18505fcd60b1e8e00f84aaf4f79cb
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-22 14:19:05 -0500

    Add links for scopes
    Chapter 39.2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   resources :genres
   root "movies#index"
+
+  get "movies/filter/:filter" => "movies#index", as: :filtered_movies
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :movies do
     resources :reviews
