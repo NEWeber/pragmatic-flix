51e886b3406740f169ce09fded34fecb71740248
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-22 13:16:50 -0500

    Define scopes for movies
    Chapter 39.1
    
    TODO: scope ordeirng isn't working as expected or described, not sure what best move is.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,22 +20,12 @@ class Movie < ApplicationRecord
   RATINGS = %W(G PG PG-13 R NC-17 #{"Not Rated"})
   validates :rating, inclusion: { in: RATINGS }
 
-  def self.released
-    where("released_on < ?", Time.now).order("released_on desc")
-  end
-
-  def self.hits
-    where(total_gross: 300_000_000..).order("total_gross desc")
-  end
-
-  def self.flops
-    where(total_gross: ..225_000_000).order("total_gross asc")
-  end
-
-  def self.recently_added
-    order("created_at desc").limit(3)
-  end
-
+  scope :released, -> { where("released_on < ?", Time.now).order("released_on desc") }
+  scope :upcoming, -> { where("released_on > ?", Time.now).order("released_on asc") }
+  scope :recent, ->(max = 5) { released.limit(max) }
+  scope :hits, -> { released.where("total_gross >= 300000000").order(total_gross: :desc) }
+  scope :flops, -> { released.where(total_gross: ..225_000_000).order("total_gross asc")
+}
 
   def flop?
     total_gross < 225_000_000 || (reviews.count >= 50 && reviews.average_stars >= 4.0)
